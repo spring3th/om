150a22ab850a0bd320cbc230c6bce21f7c9ad9d5
spring3th <dengchangqing@gmail.com>
2017-03-18 16:31:16 +0800

    second commit

--- image/admin.py
+++ image/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from image.models import Image
 
-# Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['title','slug','image','created']
+    list_filter = ['created']
+
+admin.site.register(Image,ImageAdmin)
